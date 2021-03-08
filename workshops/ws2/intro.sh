51f94ee55a0b505aacc3b691be0ee2063311cf4b
Ubuntu <student@ML-RefVm-869737.0rsh3rfi2jkurm0h1ravml0gra.ix.internal.cloudapp.net>
2021-03-08 03:00:59 +0000

    These are my assignements

---
+++ workshops/ws2/intro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 1.  Write a shell script named intro.sh to which three (3) arguments will be passed when run at the command line
+# 2.  The three (3) arguments are to be 1) your first name, 2) your age, and 3) the year you will graduate from ECU (see the image below):
+# 3.  Be sure to not use your own defined variable names; use the default shell script variables instead
+# 4.  Run the script and make sure it prints the correct output to the terminal (don’t forget to give the script execute permissions)
+
+echo "My Name is $1 and I am $2 years old and I will graduate from ECU in $3 "
+
+exit 0
