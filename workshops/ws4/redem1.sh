b7b8f0d9100386e74b549385eb38013289eb8615
Ubuntu <student@ML-RefVm-869737.0rsh3rfi2jkurm0h1ravml0gra.ix.internal.cloudapp.net>
2021-03-19 07:53:04 +0000

    These are my assignments

---
+++ workshops/ws4/redem1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Find all lines starting with "Gov"
+
+# Find all lines ending with "2013"
+
+# Find  all lines that start with a number
+
+# find all lines comprised only of a number (three methods)
